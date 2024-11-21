21f1746517bc30f5dd1285eca53d18f7cd51beb4
Tim Hunter <thunte27@uwo.ca>
2024-11-21 16:34:55 -0500

    refactor(routes): rename Create.tsx to create.tsx and update route path

---
+++ src/routes/create.tsx
@@ -0,0 +1,14 @@
+import CreateEntryForm from '@/components/entry-form'
+const Create = () => {
+  return (
+    <div className="my-8 mx-auto">
+      <CreateEntryForm />
+    </div>
+  )
+}
+
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/Create')({
+  component: Create,
+})
